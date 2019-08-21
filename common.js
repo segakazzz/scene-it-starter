401021e78a0fe49c5958c83eacfad8bb617a687f
Kazue Sasatani <segakazue@gmail.com>
2019-08-20 20:08:29 -0500

    Updated styles

--- common.js
+++ common.js
@@ -3,16 +3,18 @@ function renderMovies(movieArray, hasAddButton){
         if (currentMovie !== null){
             let moviePoster = currentMovie.Poster === 'N/A' ? 'no_image.png' : currentMovie.Poster 
             return `
-            <div class="col-12 col-md-6 col-xl-4 results">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 results">
                 <div class="movies-container">
                     <div class="movie card">
-                        <img width="100%" src="${moviePoster}">
+                        <div class="movie-poster">
+                            <img width="100%" src="${moviePoster}">
+                        </div>
                         <div class="card-body">
                             <div class="card-title">
-                                <h5 class="movie-title">${currentMovie.Title}</h5>
-                                <h5><span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+                            <h5 class="movie-title-year"><span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+                            <h5 class="movie-title">${currentMovie.Title}</h5>
                             </div>
-                            <button class="btn btn-primary ${hasAddButton ? 'btn-add' : 'btn-remove'}" 
+                            <button class="btn btn-primary btn-sm ${hasAddButton ? 'btn-add' : 'btn-remove'}" 
                             id="btn-imdbID-${currentMovie.imdbID}">${hasAddButton ? 'Add!' : 'Remove'}</button>
                         </div>
                     </div>
