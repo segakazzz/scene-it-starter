1ece8f86f1c7d254f381e7078f23a4f442f3f81b
Kazue Sasatani <segakazue@gmail.com>
2019-08-20 23:24:57 -0500

    Added more styles

--- common.js
+++ common.js
@@ -1,38 +1,46 @@
 function renderMovies(movieArray, hasAddButton) {
-  let finalHTML = movieArray.map(function(currentMovie) {
-    if (currentMovie !== null) {
-      let moviePoster =
-        currentMovie.Poster === "N/A" ? "no_image.png" : currentMovie.Poster;
-      return `
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3 results">
-                <div class="movies-container">
-                    <div class="movie card">
-                        <div class="movie-poster">
-                            <img width="100%" src="${moviePoster}">
-                        </div>
-                        <div class="card-body">
-                            <div class="card-title">
-                            <h5 class="movie-title-year"><span class="badge badge-secondary">${
-                              currentMovie.Year
-                            }</span></h5>
-                            <h5 class="movie-title">${currentMovie.Title}</h5>
-                            </div>
-                            <button class="btn btn-sm ${
-                              hasAddButton
-                                ? "btn-add btn-primary"
-                                : "btn-remove btn-secondary"
-                            }" 
-                            id="btn-imdbID-${currentMovie.imdbID}">${
-        hasAddButton ? "Add!" : "Remove"
-      }</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `;
-    }
-  });
-  return finalHTML.join("");
+  if (movieArray.length > 0 ){
+    let finalHTML = movieArray.map(function(currentMovie) {
+      if (currentMovie !== null) {
+        let moviePoster =
+          currentMovie.Poster === "N/A" ? "no_image.png" : currentMovie.Poster;
+        return `
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3 results">
+                  <div class="movies-container">
+                      <div class="movie card">
+                          <div class="movie-poster">
+                              <img width="100%" src="${moviePoster}">
+                          </div>
+                          <div class="card-body">
+                              <div class="card-title">
+                              <h5 class="movie-title-year"><span class="badge badge-secondary">${
+                                currentMovie.Year
+                              }</span></h5>
+                              <h5 class="movie-title">${currentMovie.Title}</h5>
+                              </div>
+                              <button class="btn btn-sm ${
+                                hasAddButton
+                                  ? "btn-add btn-primary"
+                                  : "btn-remove btn-secondary"
+                              }" 
+                              id="btn-imdbID-${currentMovie.imdbID}">${
+          hasAddButton ? "Add!" : "Remove"
+        }</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              `;
+      }
+    });
+    return finalHTML.join("");  
+  } else {
+    console.log('no movie found')
+    return `
+      <div id="notfound" class="col-12">
+        <h1>No movie found in the database...</h1>
+      </div>`
+  }
 }
 
 function setGranim() {
